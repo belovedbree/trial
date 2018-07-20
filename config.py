4c2effdfed33bfd2cfce768d89ab25a353a0310d
Bridget Mendoza <mendozabridget777@gmail.com>
2018-07-20 15:06:33 +0300

    Intial

---
+++ config.py
@@ -0,0 +1,19 @@
+"""Config.py"""
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = 'abcdefg'
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    # Enable debugging
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    # Enable testing
+    TESTING = True
+    DEBUG = True
